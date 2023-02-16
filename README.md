62bf7a43132110fd17a1583568cf02fb81bb2703
linyuan <673609957@qq.com>
2023-02-16 10:40:28 +0800

    chore: 初始化

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 日历服务端
 
 - [初次环境设置](https://blog.csdn.net/m0_67393413/article/details/126100779)
+- [golang的包管理工具](https://www.jianshu.com/p/760c97ff644c)
--- main.go
+++ main.go
@@ -1,16 +1,66 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var name string = "first"
 
+const (
+	host     = "localhost"
+	database = "calendar"
+	user     = "root"
+	password = "123456"
+)
+
 func main() {
 	fmt.Println("start")
-	fmt.Println(name)
 	fmt.Println("end")
 }
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 func init() {
-	name = "second"
+	// Initialize connection string.
+	var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
+
+	db, err := sql.Open("mysql", connectionString)
+	checkError(err)
+	defer db.Close()
+	err = db.Ping()
+	checkError(err)
+	fmt.Println("Successfully created connection to database.")
+
+	// Drop previous table of same name if one exists.
+	_, err = db.Exec("DROP TABLE IF EXISTS inventory;")
+	checkError(err)
+	fmt.Println("Finished dropping table (if existed).")
+
+	// Create table.
+	_, err = db.Exec("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
+	checkError(err)
+	fmt.Println("Finished creating table.")
+
+	// Insert some data into table.
+	sqlStatement, err := db.Prepare("INSERT INTO inventory (name, quantity) VALUES (?, ?);")
+	res, err := sqlStatement.Exec("banana", 150)
+	checkError(err)
+	rowCount, err := res.RowsAffected()
+	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
+
+	res, err = sqlStatement.Exec("orange", 154)
+	checkError(err)
+	rowCount, err = res.RowsAffected()
+	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
+
+	res, err = sqlStatement.Exec("apple", 100)
+	checkError(err)
+	rowCount, err = res.RowsAffected()
+	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
+	fmt.Println("Done.")
 }
