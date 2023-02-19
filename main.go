0c1f4b25c1c87b32289587d34fc3c89f1e866ce2
linyuan <673609957@qq.com>
2023-02-19 18:45:10 +0800

    chore: 数据的更新和查询

--- main.go
+++ main.go
@@ -17,8 +17,8 @@ const (
 )
 
 func main() {
-	fmt.Println("start")
-	fmt.Println("end")
+	fmt.Println("开始")
+	fmt.Println("结束")
 }
 func checkError(err error) {
 	if err != nil {
@@ -26,7 +26,13 @@ func checkError(err error) {
 	}
 }
 func init() {
-	// Initialize connection string.
+	//createData()
+	//readData()
+	updateData()
+}
+
+func createData() {
+	// 初始化
 	var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
 
 	db, err := sql.Open("mysql", connectionString)
@@ -34,33 +40,104 @@ func init() {
 	defer db.Close()
 	err = db.Ping()
 	checkError(err)
-	fmt.Println("Successfully created connection to database.")
+	fmt.Println("数据库连接创建成功")
 
-	// Drop previous table of same name if one exists.
+	// 检查是否存在 inventory，并移除已存在的 inventory表
 	_, err = db.Exec("DROP TABLE IF EXISTS inventory;")
 	checkError(err)
-	fmt.Println("Finished dropping table (if existed).")
+	fmt.Println("移除成功")
 
-	// Create table.
+	// 创建inventory表
+	// 表的属性有serial name quantity
 	_, err = db.Exec("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
 	checkError(err)
-	fmt.Println("Finished creating table.")
+	fmt.Println("表创建完成")
 
-	// Insert some data into table.
+	// 在表格中插入一些数据
 	sqlStatement, err := db.Prepare("INSERT INTO inventory (name, quantity) VALUES (?, ?);")
 	res, err := sqlStatement.Exec("banana", 150)
 	checkError(err)
 	rowCount, err := res.RowsAffected()
-	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
+	fmt.Printf("插入 %d 行 数据.\n", rowCount)
 
 	res, err = sqlStatement.Exec("orange", 154)
 	checkError(err)
 	rowCount, err = res.RowsAffected()
-	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
+	fmt.Printf("插入 %d 行 数据.\n", rowCount)
 
 	res, err = sqlStatement.Exec("apple", 100)
 	checkError(err)
 	rowCount, err = res.RowsAffected()
-	fmt.Printf("Inserted %d row(s) of data.\n", rowCount)
-	fmt.Println("Done.")
+	fmt.Printf("插入 %d 行 数据.\n", rowCount)
+	fmt.Println("完成")
+}
+
+func readData() {
+	const (
+		host     = "localhost"
+		database = "calendar"
+		user     = "root"
+		password = "123456"
+	)
+	// 初始化连接
+	var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
+	//开始建立连接
+	db, err := sql.Open("mysql", connectionString)
+	checkError(err)
+	defer db.Close()
+
+	err = db.Ping()
+	checkError(err)
+	fmt.Println("连接创建成功")
+
+	// Variables for printing column data when scanned.
+	var (
+		id       int
+		name     string
+		quantity int
+	)
+	// 读取表哥中的数据
+	rows, err := db.Query("SELECT id, name, quantity from inventory;")
+	checkError(err)
+	defer rows.Close()
+	fmt.Println("正在读取数据:")
+
+	for rows.Next() {
+		// 读取这行数据的id,name,quantity
+		err := rows.Scan(&id, &name, &quantity)
+		checkError(err)
+		fmt.Printf("行数据 = (%d, %s, %d)\n", id, name, quantity)
+	}
+
+	err = rows.Err()
+	checkError(err)
+	fmt.Println("读取数据完成.")
+
+}
+
+func updateData() {
+	const (
+		host     = "localhost"
+		database = "calendar"
+		user     = "root"
+		password = "123456"
+	)
+	// 初始化连接
+	var connectionString = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?allowNativePasswords=true", user, password, host, database)
+	//开始建立连接
+	db, err := sql.Open("mysql", connectionString)
+	checkError(err)
+	defer db.Close()
+
+	err = db.Ping()
+	checkError(err)
+	fmt.Println("连接创建成功")
+
+	// 修改表格中的数据
+	// 修改名称为apple 的数据的quantity属性值为200
+	rows, err := db.Exec("UPDATE inventory SET quantity = ? WHERE name = ?", 200, "apple")
+	checkError(err)
+	rowCount, err := rows.RowsAffected()
+	fmt.Printf("更新 %d 行数据.\n", rowCount)
+	fmt.Println("更新完成.")
 }
--- packages/helpers.go
+++ packages/helpers.go
@@ -5,3 +5,8 @@ import "fmt"
 func Helper() {
 	fmt.Println("helpers")
 }
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
