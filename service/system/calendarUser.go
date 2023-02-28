ee286d87af1fdc73295ee0f84938ec9390c91646
linyuan <673609957@qq.com>
2023-02-28 21:58:56 +0800

    feature: 日历用户表

---
+++ service/system/calendarUser.go
@@ -0,0 +1,17 @@
+package system
+
+type CalendarUserService struct {
+}
+
+func (calendarService *CalendarEventService) CreateUser() {
+
+}
+func (calendarService *CalendarEventService) GetUser() {
+
+}
+func (calendarService *CalendarEventService) UpdateUser() {
+
+}
+func (calendarService *CalendarEventService) DeleteUser() {
+
+}
