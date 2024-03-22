9cea0ee5d53971ab1820dccc324c1b57a9a6d49f
linyuan <yuanlincuc@gmail.com>
2024-03-22 10:43:04 +0800

    feature: picture save

--- system/pictureStorage.go
+++ system/pictureStorage.go
@@ -5,8 +5,8 @@ import "calendar_service/global"
 type PictureStorage struct {
 	global.CalendarEventModel `yaml:",inline"`
 	PictureId                 uint64 `json:"pictureId" gorm:"not null;unique;primary_key;comment:记录ID;size:90"`
-	ImageUrl                  uint64 `json:"imageUrl" gorm:"图片链接"`
-	UserId                    string `json:"userId" gorm:"图片所属的用户ID"`
+	UserId                    uint64 `json:"userId" gorm:"图片所属的用户ID"`
+	ImageUrl                  string `json:"imageUrl" gorm:"图片链接"`
 	PictureType               string `json:"pictureType" gorm:"图片的类型"`
 	Prompt                    string `json:"prompt" gorm:"图片的生成提示词"`
 }
