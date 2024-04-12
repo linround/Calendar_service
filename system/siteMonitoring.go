1fdf86dbce93c9e2907d59801091a75272974b5c
linyuan <yuanlincuc@gmail.com>
2024-04-12 09:52:51 +0800

    feature: 时区处理

--- system/siteMonitoring.go
+++ system/siteMonitoring.go
@@ -11,7 +11,7 @@ const (
 type SiteMonitoring struct {
 	global.CalendarEventModel `yaml:",inline"`
 	RecordID                  uint64   `json:"recordId" gorm:"not null;unique;primary_key;comment:记录ID;size:90"`
-	TimePoint                 uint64   `json:"timePoint" gorm:"访问网站的时间点"`
+	TimePoint                 string   `json:"timePoint" gorm:"访问网站的时间点"`
 	SourceIP                  string   `json:"sourceIP" gorm:"访问请求的IP"`
 	SourceSite                string   `json:"sourceSite" gorm:"当前网站来源"`
 	Category                  category `json:"category" gorm:"当前的记录的类型"`
